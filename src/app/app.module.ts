2b2056e10f1347db268eec17459c6d74eef372d3
guille1999utp <guillermo.penaranda@utp.edu.co>
2022-05-26 16:57:01 -0500

    componentes propuestos page home completos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    NavBarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
